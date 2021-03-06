3bd9cbbb58c57cb320e20cf75768fbf726eb755a
WHITEHATJR-ARUNA <71725328+WHITEHATJR-ARUNA@users.noreply.github.com>
2021-03-06 16:24:00 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ import * as firebase from 'firebase'
 require('@firebase/firestore')
 
 var firebaseConfig = {
-  apiKey: "AIzaSyA7g0I3590U5ENhDIkhYbc40m_t-NaMlJ8",
-  authDomain: "wily-app-4ff14.firebaseapp.com",
-  databaseURL: "https://wily-app-4ff14-default-rtdb.firebaseio.com",
-  projectId: "wily-app-4ff14",
-  storageBucket: "wily-app-4ff14.appspot.com",
-  messagingSenderId: "686250765046",
-  appId: "1:686250765046:web:22317c1cbe068d52dcdad7"
+  
+    apiKey: "AIzaSyAyaVICPYw_TqqCrA_V2fiGF-Bvqn4vQyw",
+    authDomain: "wily-83d4e.firebaseapp.com",
+    projectId: "wily-83d4e",
+    storageBucket: "wily-83d4e.appspot.com",
+    messagingSenderId: "367832082553",
+    appId: "1:367832082553:web:58e6dc169b1801568a8ca8",
 
 };
   // Initialize Firebase
